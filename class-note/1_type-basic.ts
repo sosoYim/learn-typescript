89e7d774c44a24cd180bfd2189eeefbce2d1cf98
sosoYim <sohyung.yim@gmail.com>
2021-12-30 22:00:27 +0900

    class-note: 타입 정의 정리

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,23 @@
+const strJs = 'hello';
+
+const strTs: string = 'hello';
+
+let num: number = 10;
+// 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10];
+let items: number[] = [1, 2, 3];
+
+// 튜플
+const address2: [string, number] = ['gangnam', 123];
+
+//객체
+let obj: object = {};
+let person: object = {
+  name: 'capt',
+  age: 100,
+};
+let person2: { name: string; age: number } = {
+  name: 'dd',
+  age: 1000,
+};
